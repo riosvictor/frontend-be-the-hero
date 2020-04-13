3ee6efdb170c08b119f6b19138e63eb29ca44b8d
Paulo Victor Rios <victorios.ba@gmail.com>
2020-04-13 06:07:42 -0300

    corringindo texto

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -45,7 +45,7 @@ export default function NewIncident() {
         <img src={logoImg} alt="Be The Hero"/>
 
           <h1>Cadastrar novo caso</h1>
-          <p>Descreva o caso detalhadamento para encontrar um herói para resolver isso.</p>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
 
           <Link className="back-link" to="/profile">
             <FiArrowLeft size={16} color="#E02041" />
